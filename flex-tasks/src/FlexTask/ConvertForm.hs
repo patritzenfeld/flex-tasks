f976841366e02ef5be9bc12516288a3124251caf
Patrick Ritzenfeld <patrick.ritzenfeld@uni-due.de>
2025-10-06 14:31:21 +0200

    add unsafe version of `getFormData` (#155)

--- flex-tasks/src/FlexTask/ConvertForm.hs
+++ flex-tasks/src/FlexTask/ConvertForm.hs
@@ -3,6 +3,7 @@
 
 module FlexTask.ConvertForm (
   getFormData,
+  unsafeGetFormData,
   ) where
 
 
@@ -12,6 +13,7 @@ import Control.Monad.Reader             (runReader)
 import Data.Map                         (fromList)
 import Data.IORef                       (readIORef, writeIORef)
 import Data.Text                        (Text)
+import System.IO.Unsafe                 (unsafePerformIO)
 import System.Log.FastLogger            (defaultBufSize, newStdoutLoggerSet)
 import Text.Blaze.Html.Renderer.String  (renderHtml)
 import Yesod
@@ -61,6 +63,22 @@ getFormData widget = do
         ^{pageBody content}|]
       return (names, (lang, concat $ lines $ renderHtml html))
 
+
+{- |
+Extract a form from the environment.
+The result is a tuple of field IDs and a map of language and internationalized html pairs.
+
+__This function employs `unsafePerformIO`!__
+It should never be used if `getFormData` is applicable instead.
+
+Intended to be used on non-randomized Autotool tasks (called "direct") where an IO context is not available.
+
+Generally, usage of this should be safe if the `Rendered Widget` argument does not incorporate `liftIO` calls.
+This will always be the case for non-custom forms.
+For custom forms, the user is responsible for making sure such calls are avoided or considered "safe".
+-}
+unsafeGetFormData :: Rendered Widget -> ([[Text]], HtmlDict)
+unsafeGetFormData = unsafePerformIO . getFormData
 
 
 -- Manipulate the request data to use a specific language.
